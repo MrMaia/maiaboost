eab274dc7b75e27585ccde090491088ba62fad1d
Allan Maia <allanmaiasantos51@hotmail.com>
2023-04-14 09:55:11 -0300

    update

---
+++ assets/js/trocaimg.js
@@ -0,0 +1,23 @@
+/*<select id="opcoes" class="form-select">
+  <option value="1">Opção 1</option>
+  <option value="2">Opção 2</option>
+  <option value="3">Opção 3</option>
+</select>
+
+<img id="imagem" src="" alt="Imagem">
+
+<script>
+  const imagens = {
+    "1": "caminho/para/imagem1.jpg",
+    "2": "caminho/para/imagem2.jpg",
+    "3": "caminho/para/imagem3.jpg",
+  };
+
+  const select = document.getElementById("opcoes");
+  const imagem = document.getElementById("imagem");
+
+  select.addEventListener("change", () => {
+    const valorOpcao = select.value;
+    imagem.src = imagens[valorOpcao];
+  });
+</script>*/
